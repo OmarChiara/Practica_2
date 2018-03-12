4cc63f65510273ec69bcfb92b191cf82a6fa18ef
RicardoEmilio <RicardoEmilio@192.168.1.71>
2018-03-12 14:01:02 -0600

    Macro de numeros LCD agregada

--- Debug/board/pin_mux.d
+++ Debug/board/pin_mux.d
@@ -1,40 +1,40 @@
 board/pin_mux.o board/pin_mux.d: ../board/pin_mux.c \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_common.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/fsl_device_registers.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/MKL25Z4.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/core_cm0plus.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/core_cmInstr.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/cmsis_gcc.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/core_cmFunc.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/system_MKL25Z4.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/MKL25Z4_features.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_clock.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_common.h \
- C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_port.h \
+ C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_common.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/fsl_device_registers.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/MKL25Z4.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/core_cm0plus.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/core_cmInstr.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/cmsis_gcc.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/core_cmFunc.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/system_MKL25Z4.h \
+ C:\Users\RicardoEmilio\git\Practica_2\CMSIS/MKL25Z4_features.h \
+ C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_clock.h \
+ C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_common.h \
+ C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_port.h \
  ../board/pin_mux.h
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_common.h:
+C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_common.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/fsl_device_registers.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/fsl_device_registers.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/MKL25Z4.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/MKL25Z4.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/core_cm0plus.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/core_cm0plus.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/core_cmInstr.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/core_cmInstr.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/cmsis_gcc.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/cmsis_gcc.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/core_cmFunc.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/core_cmFunc.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/system_MKL25Z4.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/system_MKL25Z4.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\CMSIS/MKL25Z4_features.h:
+C:\Users\RicardoEmilio\git\Practica_2\CMSIS/MKL25Z4_features.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_clock.h:
+C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_clock.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_common.h:
+C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_common.h:
 
-C:\Users\Misael\Documents\MCUXpressoIDE_10.1.1_606\workspace\Try1\Practica_2\drivers/fsl_port.h:
+C:\Users\RicardoEmilio\git\Practica_2\drivers/fsl_port.h:
 
 ../board/pin_mux.h:
--- source/Practica_2.c
+++ source/Practica_2.c
@@ -48,6 +48,7 @@
 #include "GPIO.h"
 #include "BCD.h"
 #include "LED_INDICATOR.h"
+#include "numeros.h"
 
 
 
@@ -156,16 +157,16 @@ if(GPIO_ReadPinInput(GPIOB, MASTER_RESET_BUTTON)==0U)
     {
     switch(contador)
         	{
-        	case 0 :GPIOC->PDOR= (0X40);break;
-        	case 1 :GPIOC->PDOR= (0X79);break;
-        	case 2 :GPIOC->PDOR= (0X24);break;
-        	case 3 :GPIOC->PDOR= (0X30);break;
-        	case 4 :GPIOC->PDOR= (0X19);break;
-        	case 5 :GPIOC->PDOR= (0X12);break;
-        	case 6 :GPIOC->PDOR= (0X2);break;
-        	case 7 :GPIOC->PDOR= (0X78);break;
-        	case 8 :GPIOC->PDOR= (0X0);break;
-        	case 9 :GPIOC->PDOR= (0X18);break;
+        	case 0 :GPIOC->PDOR= (cero);break;
+        	case 1 :GPIOC->PDOR= (uno);break;
+        	case 2 :GPIOC->PDOR= (dos);break;
+        	case 3 :GPIOC->PDOR= (tres);break;
+        	case 4 :GPIOC->PDOR= (cuatro);break;
+        	case 5 :GPIOC->PDOR= (cinco);break;
+        	case 6 :GPIOC->PDOR= (seis);break;
+        	case 7 :GPIOC->PDOR= (siete);break;
+        	case 8 :GPIOC->PDOR= (ocho);break;
+        	case 9 :GPIOC->PDOR= (nueve);break;
         	}
 
     }
